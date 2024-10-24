a6c39792108f5570f49c4efcdb7c1fa3ab348dec
Harry LAW-YEN <harry.lawyen@gmail.com>
2024-10-24 17:54:02 -0400

    Ajout des langues + des conversion_types pour MyFuzzer

--- TP3/CodeSource/Partie-C/my_fuzzer.py
+++ TP3/CodeSource/Partie-C/my_fuzzer.py
@@ -1,6 +1,15 @@
 import random
+import string
 
 class MyFuzzer:
+    supported_languages = [
+        'am', 'ar', 'az', 'by', 'cz', 'en', 'en_IN', 'en_NG', 'fa', 'fr',
+        'fr_CH', 'fr_BE', 'fr_DZ', 'de', 'fi', 'eo', 'es', 'es_CO', 'es_GT',
+        'es_NI', 'es_VE', 'id', 'ja', 'kn', 'ko', 'kz', 'lt', 'lv', 'pl',
+        'ro', 'ru', 'sk', 'sl', 'sr', 'sv', 'no', 'dk', 'pt', 'pt_BR', 'he',
+        'it', 'vi', 'tg', 'th', 'tr', 'nl', 'uk', 'te', 'hu', 'is'
+    ]
+    conversion_types = ['cardinal', 'ordinal', 'ordinal_num', 'year', 'currency']
 
     def _get_random_bool(self, true_weight=1, false_weight=9):
         return random.choices([True, False], weights=[true_weight, false_weight])[0]
